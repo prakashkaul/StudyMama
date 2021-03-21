2a828b8b9602b4789bcc10274ede5c34a9853063
dannellim <dannel_lim@hotmail.com>
2021-03-21 10:23:03 +0800

    first commit

---
+++ src/main/java/sg/com/studymama/StudymamaApplication.java
@@ -0,0 +1,13 @@
+package sg.com.studymama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudymamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudymamaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/com/studymama/StudymamaApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.com.studymama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudymamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
