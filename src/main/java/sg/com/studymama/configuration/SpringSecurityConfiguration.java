bb7641ce185b0247428b7faa2324e2fa60a4fbc6
dannellim <dannel_lim@hotmail.com>
2021-03-24 22:15:55 +0800

    created authenticate api generate jwt on successful login

--- src/main/java/sg/com/studymama/configuration/SpringSecurityConfiguration.java
+++ src/main/java/sg/com/studymama/configuration/SpringSecurityConfiguration.java
@@ -3,6 +3,7 @@ package sg.com.studymama.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,10 +29,19 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 	}
+	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
 
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.csrf().disable().authorizeRequests().antMatchers("/helloadmin").hasRole("ADMIN")
-				.antMatchers("/hellouser").hasAnyRole("ADMIN", "USER").and().httpBasic();
+		httpSecurity.csrf().disable().authorizeRequests()
+				.antMatchers("/helloadmin").hasRole("ADMIN")
+				.antMatchers("/hellouser").hasAnyRole("ADMIN", "USER")
+				.antMatchers("/authenticate").permitAll().anyRequest().authenticated()
+				.and().httpBasic();
 	}
 }
---
+++ src/main/java/sg/com/studymama/controller/AuthenticationController.java
@@ -0,0 +1,49 @@
+package sg.com.studymama.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import sg.com.studymama.model.AuthenticationRequest;
+import sg.com.studymama.model.AuthenticationResponse;
+import sg.com.studymama.service.CustomUserDetailsService;
+import sg.com.studymama.service.JwtUtil;
+
+@RestController
+public class AuthenticationController {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private CustomUserDetailsService userDetailsService;
+
+	@Autowired
+	private JwtUtil jwtTokenUtil;
+
+	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)
+			throws Exception {
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+					authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+		} catch (DisabledException e) {
+			throw new Exception("USER_DISABLED", e);
+		} catch (BadCredentialsException e) {
+			throw new Exception("INVALID_CREDENTIALS", e);
+		}
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String token = jwtTokenUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok(new AuthenticationResponse(token));
+	}
+}
