d994c06e3ad18c1c18f178adac9934c2a963b3c0
dongxinyi <dongxinyi@10.10.20.91>
2021-04-20 22:49:51 +0800

    post--repository, pom

---
+++ src/main/java/sg/com/studymama/repository/PostRepository.java
@@ -0,0 +1,30 @@
+package sg.com.studymama.repository;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import sg.com.studymama.DTO.PostDTO;
+import sg.com.studymama.Entity.PostEntity;
+
+
+public interface PostRepository extends JpaRepository<PostEntity, Integer> {
+	
+	final String deleteById = "delete from studymama.post p "
+			+ "where (p.post_id = ?1 ) ";
+	
+	@Modifying
+	@Transactional
+	@Query(value=deleteById, nativeQuery=true)
+	public Integer deleteByPostId(Integer postId);
+
+	public PostEntity findByPostId(Integer id);
+	
+	public List<PostEntity> findByAccountId(Integer accountId);
+
+
+}
