ed2a379ae9dfbc58e39bde77d858529b3b7d1448
HtetAung <htetaung.ycc.pku@gmail.com>
2021-04-25 20:28:47 +0800

    added paging for all request REST-API

--- src/main/java/sg/com/studymama/controller/PostController.java
+++ src/main/java/sg/com/studymama/controller/PostController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.geo.GeoPoint;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -40,40 +42,44 @@ public class PostController {
 
     @Operation(summary = "Retrieving  all posts")
     @RequestMapping(value = "/allPost", method = RequestMethod.GET)
-    public ResponseEntity<?> getAllPosts() throws Exception {
+    public ResponseEntity<?> getAllPosts(@RequestParam int currentPage,@RequestParam int pageSize) throws Exception {
         LOG.info("retrieving all Post");
+        Pageable paging = PageRequest.of(currentPage, pageSize);
 
-        return ResponseEntity.ok(springDataPostService.getAllPost());
+        return ResponseEntity.ok(springDataPostService.getAllPost(paging));
     }
 
     @Operation(summary = "Retrieving posts base on title")
     @RequestMapping(value = "/searchPostBySimilarTitle", method = RequestMethod.GET)
     public ResponseEntity<?> searchPostsBySimilarTitle(
             @Parameter(description = "The keyword to search in title")
-            @RequestParam String title) throws Exception {
+            @RequestParam String title,@RequestParam int currentPage,@RequestParam int pageSize) throws Exception {
         LOG.info("Retrieving posts base on title");
+        Pageable paging = PageRequest.of(currentPage, pageSize);
 
-        return ResponseEntity.ok(springDataPostService.searchPostByName(title));
+        return ResponseEntity.ok(springDataPostService.searchPostByName(title,paging));
     }
 
     @Operation(summary = "Retrieving posts base on category")
     @RequestMapping(value = "/searchPostByCategory", method = RequestMethod.GET)
     public ResponseEntity<?> searchPostsByCategory(
             @Parameter(description = "The category name")
-            @RequestParam String category) throws Exception {
+            @RequestParam String category,@RequestParam int currentPage,@RequestParam int pageSize) throws Exception {
         LOG.info("Retrieving posts base on category");
+        Pageable paging = PageRequest.of(currentPage, pageSize);
 
-        return ResponseEntity.ok(springDataPostService.searchPostByCategory(category));
+        return ResponseEntity.ok(springDataPostService.searchPostByCategory(category,paging));
     }
 
     @Operation(summary = "Retrieving posts base  on keyword in category")
     @RequestMapping(value = "/searchPostBySimilarCategory", method = RequestMethod.GET)
     public ResponseEntity<?> searchPostsByKeywordInCategory(
             @Parameter(description = "The keyword to search in Category ")
-            @RequestParam String keyword) throws Exception {
+            @RequestParam String keyword,@RequestParam int currentPage,@RequestParam int pageSize) throws Exception {
         LOG.info("Retrieving posts base  on keyword in category");
+        Pageable paging = PageRequest.of(currentPage, pageSize);
 
-        return ResponseEntity.ok(springDataPostService.searchPostBySimilarCategory(keyword));
+        return ResponseEntity.ok(springDataPostService.searchPostBySimilarCategory(keyword,paging));
     }
 
 
@@ -81,10 +87,11 @@ public class PostController {
     @RequestMapping(value = "/searchPostByKeywordInTitleDescCategory", method = RequestMethod.GET)
     public ResponseEntity<?> searchPostsByByKeywordInTitleDescriptionCategory(
             @Parameter(description = "The keyword to search in Title,Description and Category ")
-            @RequestParam String keyword) throws Exception {
+            @RequestParam String keyword,@RequestParam int currentPage,@RequestParam int pageSize) throws Exception {
         LOG.info("Retrieving posts base on keyword in the Title,Descriptions and Category");
+        Pageable paging = PageRequest.of(currentPage, pageSize);
 
-        return ResponseEntity.ok(springDataPostService.searchPostsByByKeywordInTitleDescriptionCategory(keyword));
+        return ResponseEntity.ok(springDataPostService.searchPostsByByKeywordInTitleDescriptionCategory(keyword,paging));
     }
 
 
@@ -93,9 +100,11 @@ public class PostController {
     public ResponseEntity<?> getAllPostsNearMe(@Parameter(description = "The value of distance you want to search ") @RequestParam Double distance,
                                                @Parameter(description = "latitude ") @RequestParam Double lat,
                                                @Parameter(description = "longitude ") @RequestParam Double lon,
-                                               @Parameter(description = "km or m ")  @RequestParam String unit) throws Exception {
+                                               @Parameter(description = "km or m ")  @RequestParam String unit,
+                                               @RequestParam int currentPage,@RequestParam int pageSize) throws Exception {
+        Pageable paging = PageRequest.of(currentPage, pageSize);
 
-        return ResponseEntity.ok(springDataPostService.searchWithin(new GeoPoint(lat,lon),distance,unit));
+        return ResponseEntity.ok(springDataPostService.searchWithin(new GeoPoint(lat,lon),distance,unit,paging));
     }
 
 }
