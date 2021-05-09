60024d37d1c3f663e43a28d3bd67d813729ff5e1
Dannel Lim Khye Hsen <56719644+dannellim@users.noreply.github.com>
2021-05-09 22:48:12 +0800

    Update CommentController.java

--- src/main/java/sg/com/studymama/controller/CommentController.java
+++ src/main/java/sg/com/studymama/controller/CommentController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import sg.com.studymama.DTO.CommentDTO;
 import sg.com.studymama.service.CommentService;
