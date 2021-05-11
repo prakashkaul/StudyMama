f83c88308536f0b73b1796c58116caaf4903d5a3
dannellim <dannel_lim@hotmail.com>
2021-05-11 15:38:34 +0800

    add init data controller

---
+++ src/main/java/sg/com/studymama/controller/DataInitController.java
@@ -0,0 +1,84 @@
+package sg.com.studymama.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.javafaker.Faker;
+
+import sg.com.studymama.DTO.CommentDTO;
+import sg.com.studymama.DTO.PostsDTO;
+import sg.com.studymama.Entity.Post;
+import sg.com.studymama.Entity.PostsEntity;
+import sg.com.studymama.model.PostData;
+import sg.com.studymama.repository.RecommendationRepository;
+import sg.com.studymama.service.PostService;
+import sg.com.studymama.service.SpringDataPostService;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class DataInitController {
+
+	private static final Logger LOG = LoggerFactory.getLogger(DataInitController.class);
+
+	@Autowired
+	private PostService postService;
+
+	@Autowired
+	private SpringDataPostService springDataPostService;
+
+	@Autowired
+	private RecommendationRepository recommendService;
+
+	@RequestMapping(value = "/initPostData", method = RequestMethod.POST)
+	public @ResponseBody ResponseEntity<PostsDTO> initPostData(@PathVariable("numberOfRows") Integer rows) {
+		PostsDTO postDTO = new PostsDTO();
+		try {
+			LOG.info("Init Post Data");
+			Faker faker = new Faker(new Locale("en-SG"));
+			postDTO = new PostsDTO();
+			for (int i = 2; i < rows + 2; i++) {
+				List<CommentDTO> comments = new ArrayList<>();
+				for (int j = 0; j < faker.number().numberBetween(1, 5); j++) {
+					CommentDTO commentDTO = new CommentDTO();
+					commentDTO.setPostId(i);
+					commentDTO.setDescription(faker.howIMetYourMother().quote());
+					commentDTO.setCommentId(j);
+				}
+				postDTO.setAccountId(faker.number().numberBetween(1, 3));
+				postDTO.setComments(comments);
+				postDTO.setCategory(faker.company().industry());
+				postDTO.setContact(faker.phoneNumber().subscriberNumber(8));
+				postDTO.setDescription(faker.address().fullAddress());
+				postDTO.setGpsX(Double.parseDouble(faker.address().longitude()));
+				postDTO.setGpsX(Double.parseDouble(faker.address().latitude()));
+				postDTO.setImages("");
+				postDTO.setPostId(i);
+				postDTO.setPrice(faker.number().numberBetween(20, 100) + "");
+				postDTO.setStatus("1");
+				postDTO.setTitle(faker.educator().course());
+				postDTO.setWebsite("www." + faker.internet().domainName());
+				PostsEntity post = postService.save(postDTO);
+				LOG.info(postDTO.toString());
+				springDataPostService.createPost(new PostData(post));
+				recommendService.save(new Post(post));// neo4j?
+			}
+		} catch (Exception e) {
+			LOG.error(e.getLocalizedMessage());
+			e.printStackTrace();
+			return null;
+		}
+		return ResponseEntity.ok(postDTO);
+	}
+}
